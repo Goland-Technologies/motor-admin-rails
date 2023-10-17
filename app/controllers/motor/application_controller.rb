7f5e372039ae133c2d314112abca5c785acb955c
Andres Abed <abedandres@gmail.com>
2023-10-17 01:33:33 -0500

    Adds goland favicon and site title

--- app/controllers/motor/application_controller.rb
+++ app/controllers/motor/application_controller.rb
@@ -6,9 +6,20 @@ module Motor
     include Motor::CurrentAbility
 
     before_action :skip_bullet, unless: -> { Rails.env == "production" }
+    before_action :set_title
 
     def skip_bullet
       Bullet.enable = false
+    end
+
+    private
+
+    def set_title
+      if !(current_user.super_admin? || current_user.admin?)
+        @title = "GoPanel - #{current_user.business.name}"
+      else
+        @title = "GoPanel - #{current_user.role.titleize}"
+      end
     end
   end
 end
